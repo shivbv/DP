0f3ec9ea050ee26636c0a300af68d564b2f887be
shivbv <shivpratap@blogvault.net>
2018-02-07 12:32:00 +0530

    added job

--- app/jobs/web_request_job.rb
+++ app/jobs/web_request_job.rb
@@ -1,22 +1,22 @@
-class WebRequestJob < Struct.new(:site_id, :url, :params, :type, :event)
+class WebRequestJob < Struct.new(:type, :url, :params, :event)
 	def logger
-		logger ||= Logger.new("#{Rails.root.to_s}/log/logfile.log")
+		logger ||= Logger.new("#{Rails.root.to_s}/log/web_requests.log")
 		identifier = "XXXX #{url} ===="
 		logger.formatter = proc { |severity, datetime, progname, msg|
-																					 "#{severity} #{datetime} #{identifier} #{msg}\n"
+						   	 "#{severity} #{datetime} #{identifier} #{msg}\n"
 		}
 		logger
 	end
 
 	def perform
-		res_file = Digest::MD5.hexdigest(url)
+		params_str = params.map{|k,v| "#{k}=#{v}"}.join(',')
+		res_file = Digest::MD5.hexdigest(url+params_str)
 		res = RestClient::Request.execute(:method => type.to_sym, :url => url, :headers => {:params => params})
 		res_code = res.code
-		File.open(res_file,'a+'){ |file|
+		File.open(res_file,'ab+'){ |file|
 			file << res.body
 		}
-		arr = [event, url, res_code, res_file]
-		WEB_REQUEST_REDIS.set(site_id, arr.inspect)
+		Resque.enqueue(event[:action], event[:id] , event[:task_id], url, response_code, res_file)
 	rescue => e
 	logger.error "REQUESTFAILED : #{e.message}" 	
 	end
