c4ba3fad4feff7cb62897576e924942d60231841
sumit-va <sumitsharma.nitsri@gmail.com>
2018-02-07 18:40:48 -0500

    output task done

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,9 @@
 class Task < ApplicationRecord
 	def self.create(category, inputfile, outputfile, total_entries)
-		Task.new(:category => category, :inputfile => inputfile, :outputfile => outputfile, :total_entries => total_entries,
-				:executed_entries => 0).save
+		task = Task.new(:category => category, :inputfile => inputfile, :outputfile => outputfile, :total_entries => total_entries,
+				:executed_entries => 0)
+		task.save
+		task
 	end
 end
 
--- lib/jobs/similar_web_response_handler_job.rb
+++ lib/jobs/similar_web_response_handler_job.rb
@@ -12,6 +12,17 @@ class SimilarWebResponseHandlerJob
 		logger
 	end
 
+	def self.parse(data)
+		global_rank = data['GlobalRank']['Rank'] if data['GlobalRank']
+		traffic = data['Engagments']['Visits']  if data['Engagments']
+		category = data['Category']
+		if data['TopCategoriesAndFills']
+			topcategories = data['TopCategoriesAndFills'].collect{ |category| category['Category'] }.join(", ")
+		end
+		toptags = data['TopTagsAndStrength'].collect{ |tag| tag['Tag']}.join(", ") if data['TopTagsAndStrength']
+		description = data['Description']
+		return global_rank, traffic, category, topcategories, toptags, description
+	end
 
 	def self.perform(task_id, swinfo_id, response_code, response_file)
 		logger = create_logger(swinfo_id)
@@ -19,24 +30,17 @@ class SimilarWebResponseHandlerJob
 		task = Task.find(task_id)
 		if response_code == 200
 			data = JSON.parse(File.read(response_file))
-			global_rank = data['GlobalRank']['Rank'] if data['GlobalRank']
-			traffic = data['Engagments']['Visits']  if data['Engagments']
-			category = data['Category']
-			if data['TopCategoriesAndFills']
-				topcategories = data['TopCategoriesAndFills'].collect{ |category| category['Category'] }.join(", ")
-			end
-			toptags = data['TopTagsAndStrength'].collect{ |tag| tag['Tag']}.join(", ") if data['TopTagsAndStrength']
-			description = data['Description']
-			swinfo.update_attributes!(:status => SimilarWebInfo::Status::EXECUTED, :global_rank => global_rank, 
+			global_rank, traffic, category, topcategories, toptags, description = parse(data)
+			swinfo.update_attributes!(:status => SimilarWebInfo::Status::PARSED, :global_rank => global_rank, 
 					:traffic => traffic, :category => category, :topcategories => topcategories, 
 					:description => description, :toptags => toptags)
 		else
 			swinfo.update_attributes!(:status => SimilarWebInfo::Status::EXECUTIONFAILED)
 		end
 		task.update_attributes!(:executed_entries => task.executed_entries + 1)
-		logger.info "SUCCESSFULLYPARSED "
-		BvLib.write_file(task_id) if task.total_entries == task.executed_entries
+		logger.info "SUCCESSFULLYPARSED"
 	rescue => e
+		swinfo.update_attributes!(:status => SimilarWebInfo::Status::PARSINGFAILED)
 		logger.error "PARSINGFAILED #{e.message}"
 	end
 end
