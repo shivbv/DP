e19620cdac984d8bbadbe04af953e8f8a7b0e4d5
shivbv <shivpratap@blogvault.net>
2018-02-13 16:25:35 +0530

    minor changes

--- app/jobs/throttler_job.rb
+++ app/jobs/throttler_job.rb
@@ -1,21 +1,21 @@
 class ThrottlerJob < ApplicationJob
-  queue_as :default
+	queue_as :default
 
-  def perform_sw()
-    while $sw_queue.length != 0
-		sw_count = 0	
-		while sw_count < 5
-    temp_array = $sw_queue.pop
-		swinfo = temp_array[0]
-		task_id = temp_array[1]
-		Resque.enqueue(WebRequestJob, 'GET', swinfo.url, {}, {'action' => 'SimilarWebResponseHandlerJob',
-		              'task_id' => task_id, 'id' => swinfo.id })
-		sw_count += 1
-		puts sw_count
+	def perform_sw()
+		while $sw_queue.length != 0
+			sw_count = 0	
+			while sw_count < 5
+				temp_array = $sw_queue.pop
+				swinfo = temp_array[0]
+				task_id = temp_array[1]
+				Resque.enqueue(WebRequestJob, 'GET', swinfo.url, {}, {'action' => 'SimilarWebResponseHandlerJob',
+									'task_id' => task_id, 'id' => swinfo.id })
+				sw_count += 1
+				puts sw_count
+			end
+			sleep(10)
 		end
-		sleep(10)
-		end
-  end
+	end
 
 	def perform_twitter()
 		while $twitter_queue.length != 0
@@ -32,5 +32,4 @@ class ThrottlerJob < ApplicationJob
 			sleep(20)
 		end
 	end
-
 end
